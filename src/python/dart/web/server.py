bedc21f4b748677d49d6455c1441edc421390a4f
Adi Ophir <aophir@rmn.com>
2016-08-17 13:31:20 -0500

    Adding https support. Certs are located in dart-config.

--- src/python/dart/web/server.py
+++ src/python/dart/web/server.py
@@ -2,4 +2,5 @@ from dart.web import app
 
 
 if __name__ == '__main__':
-    app.run(host=app.config['dart_host'], port=app.config['dart_port'], use_reloader=False)
+    context = (app.config['auth'].get('ssl_cert_path'), app.config['auth'].get('ssl_key_path'))
+    app.run(host=app.config['dart_host'], port=app.config['dart_port'], use_reloader=False, ssl_context=context)
